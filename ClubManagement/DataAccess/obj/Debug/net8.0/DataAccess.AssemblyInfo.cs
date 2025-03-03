c00e786ff2f6edaec970284808dd1ea8ecbbe154
DoThang-254 <thangmoneo2542004@gmail.com>
2025-03-03 11:47:32 +0700

    test

--- ClubManagement/DataAccess/obj/Debug/net8.0/DataAccess.AssemblyInfo.cs
+++ ClubManagement/DataAccess/obj/Debug/net8.0/DataAccess.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DataAccess")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+44262ff5d5602666aa44d6294b5de9b519c45186")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cc2e8e6f52ea60033412068d3f0924bb3b437fb3")]
 [assembly: System.Reflection.AssemblyProductAttribute("DataAccess")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DataAccess")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
